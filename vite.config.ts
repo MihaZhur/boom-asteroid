483d494dcefbd945fb7c6c917c59572c0f051c3f
Михаил Журавлев <mz1218@mail.ru>
2025-01-31 15:28:08 +0300

    fix name path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/game-asteroids/", // YOUR REPO NAME HERE
+  base: "/boom-asteroid/", // YOUR REPO NAME HERE
 });
